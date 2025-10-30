36c652b02549e50cb78dea47262acf44ae6504ad
MiguelF <miguel.barbosa.domingues@gmail.com>
2025-10-29 21:06:32 -0300

    perfil, login

--- backend-laravel/routes/web.php
+++ backend-laravel/routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\InstituicaoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
-// Rotas do CRUD de Instituições
-Route::prefix('instituicoes')->group(function () {
-    Route::get('/', [InstituicaoController::class, 'index'])->name('instituicoes.index');
-    Route::get('/create', [InstituicaoController::class, 'create'])->name('instituicoes.create');
-    Route::post('/store', [InstituicaoController::class, 'store'])->name('instituicoes.store');
-    Route::get('/{instituicao}/edit', [InstituicaoController::class, 'edit'])->name('instituicoes.edit');
-    Route::put('/{instituicao}', [InstituicaoController::class, 'update'])->name('instituicoes.update');
-    Route::delete('/{instituicao}', [InstituicaoController::class, 'destroy'])->name('instituicoes.destroy');
-});
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
